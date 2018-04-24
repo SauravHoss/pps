b317ae9ad7c11e5d6cca55c5a5fe48dfc4ab79d8
Rome <Rome@Rome>
2018-04-24 17:58:13 -0400

    starts

---
+++ KittyKat/src/RunningLayout.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RunningLayout extends Application
+{
+
+	//Fields
+	Scene questionpage, clickerpage, finalpage;
+	
+	
+	@Override
+	public void start(Stage stage1) throws Exception 
+	{
+		
+	}
+
+}
